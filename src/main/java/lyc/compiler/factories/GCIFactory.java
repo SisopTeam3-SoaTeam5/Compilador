e61667864dc38a0372b221dceec48b1ca4287551
Jsesan <jsesan00@gmail.com>
2022-10-23 18:28:48 -0300

    tercetos estructura

---
+++ src/main/java/lyc/compiler/factories/GCIFactory.java
@@ -0,0 +1,17 @@
+package lyc.compiler.factories;
+
+import lyc.compiler.model.Terceto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GCIFactory {
+    private ArrayList<Terceto> tercetos = new ArrayList<>() ;
+    private HashMap<String, Terceto> GCIVariables = new HashMap<>();
+
+    private void insertarTerceto(Terceto t){
+        tercetos.add(t);
+    }
+
+
+}
